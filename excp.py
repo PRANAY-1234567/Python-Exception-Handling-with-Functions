d30eedbe833931cb4290130efb44c06ff37d596b
Pranay <pranayvjadhao197@gmail.com>
2026-06-07 22:07:25 +0530

    Initial commit

---
+++ excp.py
@@ -0,0 +1,21 @@
+def division(x, y):
+    try:
+        d = x // y  # Integer division like Java
+        print("Division is", d)
+
+    except ZeroDivisionError:
+        print("Error in data : Divisor should not be zero")
+
+
+print("Program starts...")
+
+try:
+    a = int(input("Enter first number : "))
+    b = int(input("Enter second number : "))
+
+    division(a, b)
+
+except ValueError:
+    print("Error in data : Number not provided correctly")
+
+print("Program ends...")
